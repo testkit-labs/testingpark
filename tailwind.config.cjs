b926f868d924ae720b2d037a185d42c8c21a7a84
Yassine Zeriouh <y@zeriouh.io>
2022-09-14 16:33:05 +0200

    feat(theme): add tailwind and flowbite

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+const config = {
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
+	],
+
+  darkMode: 'class',
+
+	theme: {
+		extend: {}
+	},
+
+	plugins: [require('flowbite/plugin')]
+};
+
+module.exports = config;
