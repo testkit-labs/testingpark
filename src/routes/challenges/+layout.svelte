940aeaef1132446b07bdb772230072b7b3e17f35
Yassine Zeriouh <y@zeriouh.io>
2022-09-22 01:44:53 +0200

    feat(challenges): add back to home button

---
+++ src/routes/challenges/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { Button } from "flowbite-svelte"
+  import { Home } from "svelte-heros-v2"
+</script>
+
+<div class="mb-5">
+  <Button color="alternative" size="xs" href="/">
+    <Home class="h-4 mr-1 -ml-1" />
+    Back to home
+  </Button>
+</div>
+
+<slot />
