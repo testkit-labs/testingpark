0f062fada2e08f7bf5b4abf2344a15987d658799
Yassine Zeriouh <y@zeriouh.io>
2022-09-22 01:40:31 +0200

    feat(challenges): Add basic typing challenges (wip)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,29 @@
 <script>
   import { Heading, Hr, P } from "flowbite-svelte"
-  import { CursorArrowRays } from "svelte-heros-v2"
+  import { CursorArrowRays, PencilSquare } from "svelte-heros-v2"
   import ChallengeCard from "../components/ChallengeCard.svelte"
 </script>
 
 <Heading class="mb-2">Testing Park</Heading>
 <P class="max-w-2xl">
   This is a playground to practice and challenge your testing skills. You can choose one of the challenges below and
-  create a test for it. At <a class="text-blue-700 hover:text-blue-500 dark:text-blue-500 dark:hover:text-blue-400" href="https://testkit.app/?ref=testingpark_p">
+  create a test for it. At <a
+    class="text-blue-700 hover:text-blue-500 dark:text-blue-500 dark:hover:text-blue-400"
+    href="https://testkit.app/?ref=testingpark_p">
     Testkit,
   </a>
   we use this tool to test the functionality of our
-  <a class="text-blue-700 hover:text-blue-500 dark:text-blue-500 dark:hover:text-blue-400" href="https://cypress-recorder.com" target="_blank">Cypress Recorder</a>
+  <a
+    class="text-blue-700 hover:text-blue-500 dark:text-blue-500 dark:hover:text-blue-400"
+    href="https://cypress-recorder.com"
+    target="_blank">
+    Cypress Recorder
+  </a>
   and our main application. The code is
-  <a class="text-blue-700 hover:text-blue-500 dark:text-blue-500 dark:hover:text-blue-400" href="https://github.com/testkit-labs/testingpark" target="_blank">
+  <a
+    class="text-blue-700 hover:text-blue-500 dark:text-blue-500 dark:hover:text-blue-400"
+    href="https://github.com/testkit-labs/testingpark"
+    target="_blank">
     fully open source on GitHub.
   </a>
 </P>
@@ -21,10 +31,13 @@
 <Hr class="mb-6" />
 
 <Heading tag="h4" class="mb-3">Difficulty: Easy</Heading>
-<ChallengeCard href="/challenges/easy/clicking" title="Clicking" icon={CursorArrowRays}>
-  Click some buttons and links, and check if you get the correct output
-</ChallengeCard>
+
+<div class="grid gap-8 grid-cols-1 auto-rows-auto sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 3xl:grid-cols-5">
+  <ChallengeCard href="/challenges/easy/clicking" title="Clicking" icon={CursorArrowRays}>
+    Click some buttons and links, and check if you get the correct output
+  </ChallengeCard>
 
   <ChallengeCard href="/challenges/easy/typing" title="Typing" icon={PencilSquare}>
     Type some text into different types of inputs and check if you get the correct output
   </ChallengeCard>
+</div>
---
+++ src/routes/challenges/easy/typing/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { Input, Label } from "flowbite-svelte"
+</script>
+
+<div class="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 3xl:grid-cols-6 auto-rows-auto">
+  <div>
+    <Label for="text-input">Text Input</Label>
+    <Input type="text" id="text-input" />
+  </div>
+
+  <div>
+    <Label for="password-input">Password Input</Label>
+    <Input type="password" id="password-input" />
+  </div>
+  
+  <div>
+    <Label for="contenteditable">Contenteditable</Label>
+    <div id="contenteditable" class="h-10 px-3 py-1.5 border rounded-lg bg-gray-50 dark:bg-gray-700 border-gray-300 dark:border-gray-600" contenteditable></div>
+  </div>
+</div>
