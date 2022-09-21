09b4639436515f7094c7a96e3b69bf504c341ef8
Yassine Zeriouh <y@zeriouh.io>
2022-09-21 18:07:47 +0200

    Adjust index page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { Card, Heading, Hr, P } from "flowbite-svelte"
+  import { Heading, Hr, P } from "flowbite-svelte"
   import { CursorArrowRays } from "svelte-heros-v2"
   import ChallengeCard from "../components/ChallengeCard.svelte"
 </script>
@@ -7,13 +7,13 @@
 <Heading class="mb-2">Testing Park</Heading>
 <P class="max-w-2xl">
   This is a playground to practice and challenge your testing skills. You can choose one of the challenges below and
-  create a test for it. At <a class="text-blue-700 hover:text-blue-500" href="https://testkit.app/?ref=testingpark_p">
+  create a test for it. At <a class="text-blue-700 hover:text-blue-500 dark:text-blue-500 dark:hover:text-blue-400" href="https://testkit.app/?ref=testingpark_p">
     Testkit,
   </a>
   we use this tool to test the functionality of our
-  <a class="text-blue-700 hover:text-blue-500" href="https://cypress-recorder.com" target="_blank">Cypress Recorder</a>
+  <a class="text-blue-700 hover:text-blue-500 dark:text-blue-500 dark:hover:text-blue-400" href="https://cypress-recorder.com" target="_blank">Cypress Recorder</a>
   and our main application. The code is
-  <a class="text-blue-700 hover:text-blue-500" href="https://github.com/testkit-labs/testingpark" target="_blank">
+  <a class="text-blue-700 hover:text-blue-500 dark:text-blue-500 dark:hover:text-blue-400" href="https://github.com/testkit-labs/testingpark" target="_blank">
     fully open source on GitHub.
   </a>
 </P>
