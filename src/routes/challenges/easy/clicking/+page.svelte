80afecec966a118199a5470c6b17bf1b1bb90297
Yassine Zeriouh <y@zeriouh.io>
2022-09-21 23:13:24 +0200

    feat(clicking): implement challenge (wip)

---
+++ src/routes/challenges/easy/clicking/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { Button } from "flowbite-svelte"
+
+  let clicked = false
+
+  const onClick = () => {
+    clicked = true
+    setTimeout(() => {
+      clicked = false
+    }, 3000)
+  }
+</script>
+
+<Button on:click={onClick} color={clicked ? "green" : "blue"}>
+  {#if clicked}
+    I've been clicked!
+  {:else}
+    Click me!
+  {/if}
+</Button>
