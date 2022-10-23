5386d36d151907ca7ef949d6939d34ef3c79375c
Yassine Zeriouh <y@zeriouh.io>
2022-10-23 02:50:56 +0200

    Add uploading page

---
+++ src/routes/challenges/easy/uploading/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { Fileupload, Label } from "flowbite-svelte"
+  import Dropzone from "svelte-file-dropzone"
+
+  let files = {
+    accepted: [],
+    rejected: []
+  };
+
+  function handleFilesSelect(e) {
+    const { acceptedFiles, fileRejections } = e.detail;
+    files.accepted = [...files.accepted, ...acceptedFiles];
+    files.rejected = [...files.rejected, ...fileRejections];
+  }
+</script>
+
+<Label for="simple" class="mb-0.5">Simple File Input</Label>
+<Fileupload id="simple" />
+
+<Dropzone on:drop={handleFilesSelect} class="h-96" />
+<ol>
+  {#each files.accepted as item}
+    <li>{item.name}</li>
+  {/each}
+</ol>
